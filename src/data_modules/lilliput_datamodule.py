46d6c68a6e7fd6d04cbcffc2916f47b6b17c73f2
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-01-21 17:08:48 +0000

    keep data generation the same

--- src/data_modules/lilliput_datamodule.py
+++ src/data_modules/lilliput_datamodule.py
@@ -21,7 +21,7 @@ class LilliputDataModule(BaseDataModule):
         self.alpha = cfg.alpha
         self._cf_available = True
         self.gamma = cfg.gamma
-        self.seed = 8  # cfg.seed
+        self.seed = cfg.seed
         self.num_samples = cfg.num_samples
         self.train_dims = None
         self.num_workers = cfg.num_workers
@@ -31,7 +31,7 @@ class LilliputDataModule(BaseDataModule):
     def prepare_data(self) -> None:
         # called only on 1 GPU
         dataset, factual_data, cf_data, data_true_outcome, best_guess, s0s0, s0s1, s1s0, s1s1 = lilliput(
-            seed=self.seed, alpha=self.alpha, num_samples=self.num_samples
+            seed=8, alpha=self.alpha, num_samples=self.num_samples
         )
         self.best_guess = best_guess
         self.s0_s0 = s0s0
