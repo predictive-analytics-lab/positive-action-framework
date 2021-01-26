4d2b377543536d3e71acda5f3a95fe01d8300cd3
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-01-26 21:52:00 +0000

    better plotting of recons

--- src/datasets/adult_dataset.py
+++ src/datasets/adult_dataset.py
@@ -7,4 +7,4 @@ from ethicml import Dataset, DataTuple, adult
 def adult_data(*, bin_nationality: bool) -> Tuple[Dataset, DataTuple]:
     """Generate very simple X data."""
     dataset = adult(binarize_nationality=bin_nationality)
-    return dataset, dataset.load()
+    return dataset, dataset.load(ordered=True)
