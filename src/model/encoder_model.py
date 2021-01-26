dc6e402bebb1c49efa397b997a7a5cf7f7179111
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-01-26 17:47:24 +0000

    better encoder loss for discrete features

--- src/model/encoder_model.py
+++ src/model/encoder_model.py
@@ -157,12 +157,15 @@ class AE(CommonModel):
                 x[:, slice(self.feature_groups["discrete"][-1].stop, x.shape[1])],
                 reduction="mean",
             )
+            _tmp_recon_loss = torch.tensor(0)
             for group_slice in self.feature_groups["discrete"]:
-                recon_loss += cross_entropy(
+                _tmp_recon_loss += cross_entropy(
                     index_by_s(recons, s)[:, group_slice],
                     torch.argmax(x[:, group_slice], dim=-1),
                     reduction="mean",
                 )
+            recon_loss += _tmp_recon_loss / len(self.feature_groups["discrete"])
+            recon_loss /= 2
         else:
             recon_loss = mse_loss(index_by_s(recons, s), x, reduction="mean")
 
