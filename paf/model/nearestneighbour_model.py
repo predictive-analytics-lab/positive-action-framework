18fba953d862a68de7f2ce4e7d74dd753a883519
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-07-26 14:11:37 +0100

    add the files

---
+++ paf/model/nearestneighbour_model.py
@@ -0,0 +1,92 @@
+from typing import Optional, Union
+
+import pandas as pd
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.types import EPOCH_OUTPUT, STEP_OUTPUT
+import torch
+from torch import Tensor, nn
+import torch.nn.functional as F
+
+from paf.base_templates.dataset_utils import Batch, CfBatch
+
+
+class NearestNeighbourModel(pl.LightningModule):
+    def __init__(self, clf_model: nn.Module, data: pl.LightningDataModule):
+        super().__init__()
+        self.clf = clf_model
+        self.train_features = torch.tensor(data.train_data.x.values)
+        self.train_sens = torch.tensor(data.train_data.s.values)
+        self.train_labels = torch.tensor(data.train_data.y.values)
+
+        self.train_features = nn.Parameter(
+            F.normalize(self.train_features.detach(), dim=1, p=2), requires_grad=False
+        ).float()
+        self.train_labels = nn.Parameter(self.train_labels.detach(), requires_grad=False).float()
+
+    def forward(self, test_features: Tensor, sens_label: Tensor) -> Tensor:
+        test_features = F.normalize(test_features, dim=1, p=2)
+
+        features = []
+        labels = []
+
+        for point, s_label in zip(test_features, sens_label):
+            sim = point @ self.train_features[(self.train_sens != s_label).squeeze(-1)].t()
+            features.append(
+                self.train_features[(self.train_sens != s_label).squeeze(-1)][sim.argmax(-1)]
+            )
+            labels.append(
+                self.train_labels[(self.train_sens != s_label).squeeze(-1)][sim.argmax(-1)]
+            )
+
+        return torch.stack(features, dim=0), torch.stack(labels, dim=0)
+
+    def training_step(self, *args, **kwargs) -> STEP_OUTPUT:
+        ...
+
+    def test_step(self, batch: Union[Batch, CfBatch], batch_idx) -> Optional[STEP_OUTPUT]:
+        cf_feats, cf_outcome = self(batch.x, batch.s)
+        preds = self.clf(batch.x)
+
+        return {
+            "cf_preds": cf_outcome,
+            "preds": preds,
+            "sens": batch.s,
+            "x": batch.x,
+            "s": batch.s,
+            "y": batch.y,
+        }
+
+    def test_epoch_end(self, outputs: EPOCH_OUTPUT) -> None:
+        self.all_preds = torch.cat([_r["preds"] for _r in outputs], 0)
+        self.all_cf_preds = torch.cat([_r["cf_preds"] for _r in outputs], 0)
+        self.all_sens = torch.cat([_r["sens"] for _r in outputs], 0)
+
+        self.all_s = torch.cat([_r["s"] for _r in outputs], 0)
+        self.all_x = torch.cat([_r["x"] for _r in outputs], 0)
+        self.all_y = torch.cat([_r["y"] for _r in outputs], 0)
+        stacked = []
+        for _s, p, cfp in zip(self.all_sens, self.all_preds, self.all_cf_preds):
+            if _s == 0:
+                stacked.append((p, cfp))
+            else:
+                stacked.append((cfp, p))
+
+        stacked = torch.tensor(stacked)
+
+        self.pd_results = pd.DataFrame(
+            torch.cat(
+                [
+                    (stacked[:, 0] >= 0).unsqueeze(-1).long(),
+                    (stacked[:, 1] >= 0).unsqueeze(-1).long(),
+                    self.all_sens.unsqueeze(-1).long(),
+                    (self.all_preds >= 0).long(),
+                ],
+                dim=1,
+            )
+            .cpu()
+            .numpy(),
+            columns=["s1_0_s2_0", "s1_1_s2_1", "true_s", "actual"],
+        )
+
+    def configure_optimizers(self):
+        ...
---
+++ tests/model_test.py
@@ -0,0 +1,52 @@
+from typing import Final, List
+
+from hydra import compose, initialize
+from hydra.utils import instantiate
+from omegaconf import OmegaConf
+
+from paf.main import Config, run_aies
+
+CFG_PTH: Final[str] = "../paf/configs"
+SCHEMAS: Final[List[str]] = [
+    "enc=basic",
+    "clf=basic",
+    "exp=unit_test",
+    "trainer=unit_test",
+]
+
+
+def test_enc(dm_schema: str) -> None:
+    """Test the encoder network runs."""
+    with initialize(config_path=CFG_PTH):
+        # config is relative to a module
+        hydra_cfg = compose(
+            config_name="base_conf",
+            overrides=[f"data={dm_schema}"] + SCHEMAS,
+        )
+        cfg: Config = instantiate(hydra_cfg, _recursive_=True, _convert_="partial")
+
+        cfg.data.prepare_data()
+        encoder = cfg.enc
+        encoder.build(
+            num_s=cfg.data.num_s,
+            data_dim=cfg.data.data_dim,
+            s_dim=cfg.data.s_dim,
+            cf_available=cfg.data.cf_available,
+            feature_groups=cfg.data.feature_groups,
+            outcome_cols=cfg.data.column_names,
+            scaler=cfg.data.scaler,
+        )
+        cfg.trainer.fit(model=encoder, datamodule=cfg.data)
+        cfg.trainer.test(model=encoder, ckpt_path=None, datamodule=cfg.data)
+
+
+def test_nn() -> None:
+    """Quick run on models to check nothing's broken."""
+    with initialize(config_path=CFG_PTH):
+        # config is relative to a module
+        hydra_cfg = compose(
+            config_name="base_conf",
+            overrides=[f"data=lill"] + SCHEMAS + [f"exp.model=nn"],
+        )
+        cfg: Config = instantiate(hydra_cfg, _recursive_=True, _convert_="partial")
+        run_aies(cfg, raw_config=OmegaConf.to_container(hydra_cfg, resolve=True, enum_to_str=True))
