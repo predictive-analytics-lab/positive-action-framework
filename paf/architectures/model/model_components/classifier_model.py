74399b42df2ae979618a127089ccdd5744810561
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-11-04 17:48:21 +0000

    conditional decoder

--- paf/architectures/model/model_components/classifier_model.py
+++ paf/architectures/model/model_components/classifier_model.py
@@ -167,16 +167,22 @@ class Clf(CommonModel):
             hid_multiplier=self.latent_multiplier,
             weight=self._adv_weight,
         )
-        self.decoders = nn.ModuleList(
-            [
-                Decoder(
-                    latent_dim=self.latent_dims,
-                    in_size=1,
-                    blocks=self.decoder_blocks,
-                    hid_multiplier=self.latent_multiplier,
-                )
-                for _ in range(num_s)
-            ]
+        # self.decoders = nn.ModuleList(
+        #     [
+        #         Decoder(
+        #             latent_dim=self.latent_dims,
+        #             in_size=1,
+        #             blocks=self.decoder_blocks,
+        #             hid_multiplier=self.latent_multiplier,
+        #         )
+        #         for _ in range(num_s)
+        #     ]
+        # )
+        self.decoders = Decoder(
+            latent_dim=self.latent_dims + s_dim,
+            in_size=1,
+            blocks=self.decoder_blocks,
+            hid_multiplier=self.latent_multiplier,
         )
         self.built = True
 
@@ -188,8 +194,8 @@ class Clf(CommonModel):
         s_pred = self.adv.forward(z)
         # preds = [dec(z) for dec in self.decoders]
         preds = [
-            dec(z)  # torch.cat([z, torch.ones_like(s[..., None]) * i], dim=1))
-            for i, dec in enumerate(self.decoders)
+            self.decoders(torch.cat([z, torch.ones_like(s[..., None]) * i], dim=1))
+            for i in range(2)
         ]
         return ClfFwd(z=z, s=s_pred, y=preds)
 
