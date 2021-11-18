1f84b83c27deb8b73d38daf80feae566bcefde3b
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-11-18 23:09:04 +0000

    cyclegan improvements

--- paf/architectures/model/model_components/classifier_model.py
+++ paf/architectures/model/model_components/classifier_model.py
@@ -226,56 +226,56 @@ class Clf(CommonModel):
     def training_step(self, batch: Batch | CfBatch | TernarySample, *_: Any) -> Tensor:
         assert self.built
 
-        x_s0y0 = self.pool_x_s0y0.push_and_pop(batch.x[(batch.s == 0) & (batch.y == 0)])
-        x_s0y1 = self.pool_x_s0y1.push_and_pop(batch.x[(batch.s == 0) & (batch.y == 1)])
-        assert len(x_s0y0) == len(x_s0y1)
+        # x_s0y0 = self.pool_x_s0y0.push_and_pop(batch.x[(batch.s == 0) & (batch.y == 0)])
+        # x_s0y1 = self.pool_x_s0y1.push_and_pop(batch.x[(batch.s == 0) & (batch.y == 1)])
+        # assert len(x_s0y0) == len(x_s0y1)
+        #
+        # s_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
+        # s_s0y1 = batch.x.new_zeros((x_s0y0.shape[0]))
+        # assert len(s_s0y0) == len(s_s0y1)
+        #
+        # y_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
+        # y_s0y1 = batch.x.new_ones((x_s0y1.shape[0]))
+        # x_s1y0 = self.pool_x_s1y0.push_and_pop(batch.x[(batch.s == 1) & (batch.y == 0)])
+        # x_s1y1 = self.pool_x_s1y1.push_and_pop(batch.x[(batch.s == 1) & (batch.y == 1)])
+        # s_s1y0 = batch.x.new_ones((x_s0y1.shape[0]))
+        # s_s1y1 = batch.x.new_ones((x_s0y1.shape[0]))
+        # y_s1y0 = batch.x.new_zeros((x_s0y0.shape[0]))
+        # y_s1y1 = batch.x.new_ones((x_s0y1.shape[0]))
+        #
+        # x_s0 = torch.cat([x_s0y0, x_s0y1], dim=0)
+        # s_s0 = torch.cat([s_s0y0, s_s0y1], dim=0)
+        # y_s0 = torch.cat([y_s0y0, y_s0y1], dim=0)
+        # mixed_s0 = self.mixup_s0(x_s0, targets=y_s0.long())
+        #
+        # x_s1 = torch.cat([x_s1y0, x_s1y1], dim=0)
+        # s_s1 = torch.cat([s_s1y0, s_s1y1], dim=0)
+        # y_s1 = torch.cat([y_s1y0, y_s1y1], dim=0)
+        # mixed_s1 = self.mixup_s1(x_s1, targets=y_s1.long())
+        #
+        # mixed_x = torch.cat([mixed_s0.inputs, mixed_s1.inputs], dim=0)
+        # s = torch.cat([s_s0, s_s1], dim=0)
+        # mixed_y = torch.cat([mixed_s0.targets[:, 1], mixed_s1.targets[:, 1]], dim=0)
+        #
+        # x = torch.cat([x_s0, x_s1], dim=0)
+        # y = torch.cat([y_s0, y_s1], dim=0)
 
-        s_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
-        s_s0y1 = batch.x.new_zeros((x_s0y0.shape[0]))
-        assert len(s_s0y0) == len(s_s0y1)
-
-        y_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
-        y_s0y1 = batch.x.new_ones((x_s0y1.shape[0]))
-        x_s1y0 = self.pool_x_s1y0.push_and_pop(batch.x[(batch.s == 1) & (batch.y == 0)])
-        x_s1y1 = self.pool_x_s1y1.push_and_pop(batch.x[(batch.s == 1) & (batch.y == 1)])
-        s_s1y0 = batch.x.new_ones((x_s0y1.shape[0]))
-        s_s1y1 = batch.x.new_ones((x_s0y1.shape[0]))
-        y_s1y0 = batch.x.new_zeros((x_s0y0.shape[0]))
-        y_s1y1 = batch.x.new_ones((x_s0y1.shape[0]))
-
-        x_s0 = torch.cat([x_s0y0, x_s0y1], dim=0)
-        s_s0 = torch.cat([s_s0y0, s_s0y1], dim=0)
-        y_s0 = torch.cat([y_s0y0, y_s0y1], dim=0)
-        mixed_s0 = self.mixup_s0(x_s0, targets=y_s0.long())
-
-        x_s1 = torch.cat([x_s1y0, x_s1y1], dim=0)
-        s_s1 = torch.cat([s_s1y0, s_s1y1], dim=0)
-        y_s1 = torch.cat([y_s1y0, y_s1y1], dim=0)
-        mixed_s1 = self.mixup_s1(x_s1, targets=y_s1.long())
-
-        mixed_x = torch.cat([mixed_s0.inputs, mixed_s1.inputs], dim=0)
-        s = torch.cat([s_s0, s_s1], dim=0)
-        mixed_y = torch.cat([mixed_s0.targets[:, 1], mixed_s1.targets[:, 1]], dim=0)
-
-        x = torch.cat([x_s0, x_s1], dim=0)
-        y = torch.cat([y_s0, y_s1], dim=0)
-
-        mixed_out = self.forward(x=mixed_x, s=s)
-        mixed_pred_loss = torch.nn.functional.mse_loss(
-            index_by_s(mixed_out.y, s).squeeze().sigmoid(), mixed_y
-        )
-
-        clf_out = self.forward(x=x, s=s)
-        _iw = batch.iw if self.use_iw and isinstance(batch, (Batch, CfBatch)) else None
-        pred_loss = self.loss.pred_loss(clf_out, s=s, y=y, weight=_iw)
-        adv_loss = self.loss.adv_loss(clf_out, s=s)
-        mmd_loss = self.loss.mmd_loss(clf_out, s=s)
-
-        # mixed = self.mixup(x, targets=y.long(), group_labels=s.long())
-        # mixed_out = self.forward(x=mixed.inputs, s=torch.zeros_like(s))
-        # mixed_pred_loss = torch.nn.functional.binary_cross_entropy_with_logits(
-        #     index_by_s(mixed_out.y, s).squeeze(), mixed.targets[:, 1]
+        # mixed_out = self.forward(x=batch.x, s=batch.s)
+        # mixed_pred_loss = torch.nn.functional.mse_loss(
+        #     index_by_s(mixed_out.y, s).squeeze().sigmoid(), mixed_y
         # )
+
+        clf_out = self.forward(x=batch.x, s=batch.s)
+        _iw = batch.iw if self.use_iw and isinstance(batch, (Batch, CfBatch)) else None
+        pred_loss = self.loss.pred_loss(clf_out, s=batch.s, y=batch.y, weight=_iw)
+        adv_loss = self.loss.adv_loss(clf_out, s=batch.s)
+        mmd_loss = self.loss.mmd_loss(clf_out, s=batch.s)
+
+        mixed = self.mixup(batch.x, targets=batch.y.long(), group_labels=batch.s.long())
+        mixed_out = self.forward(x=mixed.inputs, s=torch.zeros_like(batch.s))
+        mixed_pred_loss = torch.nn.functional.mse_loss(
+            index_by_s(mixed_out.y, batch.s).squeeze().sigmoid(), mixed.targets[:, 1]
+        )
 
         loss = pred_loss + adv_loss + mmd_loss + mixed_pred_loss
 
