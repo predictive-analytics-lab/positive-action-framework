803a18b744124bdcffd9bf3f8685b485ec687a32
Oliver Thomas <1835492+olliethomas@users.noreply.github.com>
2021-11-19 13:23:56 +0000

    cyclegan improvements

--- paf/architectures/model/model_components/classifier_model.py
+++ paf/architectures/model/model_components/classifier_model.py
@@ -134,15 +134,15 @@ class Clf(CommonModel):
             kernel=str_to_enum(mmd_kernel, enum=KernelType),
         )
 
-        # self.mixup = RandomMixUp(
+        self.mixup = RandomMixUp(
+            lambda_sampler=torch.distributions.Uniform(0.0, 0.49), num_classes=2
+        )
+        # self.mixup_s0 = RandomMixUp(
         #     lambda_sampler=torch.distributions.Uniform(0.0, 1.0), num_classes=2
         # )
-        self.mixup_s0 = RandomMixUp(
-            lambda_sampler=torch.distributions.Uniform(0.0, 1.0), num_classes=2
-        )
-        self.mixup_s1 = RandomMixUp(
-            lambda_sampler=torch.distributions.Uniform(0.0, 1.0), num_classes=2
-        )
+        # self.mixup_s1 = RandomMixUp(
+        #     lambda_sampler=torch.distributions.Uniform(0.0, 1.0), num_classes=2
+        # )
 
         self.pool_x_s0y0 = HistoryPool(
             pool_size=batch_size // 4
@@ -237,39 +237,39 @@ class Clf(CommonModel):
     def training_step(self, batch: Batch | CfBatch | TernarySample, *_: Any) -> Tensor:
         assert self.built
 
-        x_s0y0 = self.pool_x_s0y0.push_and_pop(batch.x[(batch.s == 0) & (batch.y == 0)])
-        x_s0y1 = self.pool_x_s0y1.push_and_pop(batch.x[(batch.s == 0) & (batch.y == 1)])
+        # x_s0y0 = self.pool_x_s0y0.push_and_pop(batch.x[(batch.s == 0) & (batch.y == 0)])
+        # x_s0y1 = self.pool_x_s0y1.push_and_pop(batch.x[(batch.s == 0) & (batch.y == 1)])
         # assert len(x_s0y0) == len(x_s0y1)
 
-        s_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
-        s_s0y1 = batch.x.new_zeros((x_s0y1.shape[0]))
+        # s_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
+        # s_s0y1 = batch.x.new_zeros((x_s0y1.shape[0]))
         # assert len(s_s0y0) == len(s_s0y1)
 
-        y_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
-        y_s0y1 = batch.x.new_ones((x_s0y1.shape[0]))
-        x_s1y0 = self.pool_x_s1y0.push_and_pop(batch.x[(batch.s == 1) & (batch.y == 0)])
-        x_s1y1 = self.pool_x_s1y1.push_and_pop(batch.x[(batch.s == 1) & (batch.y == 1)])
-        s_s1y0 = batch.x.new_ones((x_s1y0.shape[0]))
-        s_s1y1 = batch.x.new_ones((x_s1y1.shape[0]))
-        y_s1y0 = batch.x.new_zeros((x_s1y0.shape[0]))
-        y_s1y1 = batch.x.new_ones((x_s1y1.shape[0]))
+        # y_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
+        # y_s0y1 = batch.x.new_ones((x_s0y1.shape[0]))
+        # x_s1y0 = self.pool_x_s1y0.push_and_pop(batch.x[(batch.s == 1) & (batch.y == 0)])
+        # x_s1y1 = self.pool_x_s1y1.push_and_pop(batch.x[(batch.s == 1) & (batch.y == 1)])
+        # s_s1y0 = batch.x.new_ones((x_s1y0.shape[0]))
+        # s_s1y1 = batch.x.new_ones((x_s1y1.shape[0]))
+        # y_s1y0 = batch.x.new_zeros((x_s1y0.shape[0]))
+        # y_s1y1 = batch.x.new_ones((x_s1y1.shape[0]))
 
-        x_s0 = torch.cat([x_s0y0, x_s0y1], dim=0)
-        s_s0 = torch.cat([s_s0y0, s_s0y1], dim=0)
-        y_s0 = torch.cat([y_s0y0, y_s0y1], dim=0)
+        # x_s0 = torch.cat([x_s0y0, x_s0y1], dim=0)
+        # s_s0 = torch.cat([s_s0y0, s_s0y1], dim=0)
+        # y_s0 = torch.cat([y_s0y0, y_s0y1], dim=0)
         # mixed_s0 = self.mixup_s0(x_s0, targets=y_s0.long())
 
-        x_s1 = torch.cat([x_s1y0, x_s1y1], dim=0)
-        s_s1 = torch.cat([s_s1y0, s_s1y1], dim=0)
-        y_s1 = torch.cat([y_s1y0, y_s1y1], dim=0)
+        # x_s1 = torch.cat([x_s1y0, x_s1y1], dim=0)
+        # s_s1 = torch.cat([s_s1y0, s_s1y1], dim=0)
+        # y_s1 = torch.cat([y_s1y0, y_s1y1], dim=0)
         # mixed_s1 = self.mixup_s1(x_s1, targets=y_s1.long())
 
         # mixed_x = torch.cat([mixed_s0.inputs, mixed_s1.inputs], dim=0)
-        s = torch.cat([s_s0, s_s1], dim=0)
+        # s = torch.cat([s_s0, s_s1], dim=0)
         # mixed_y = torch.cat([mixed_s0.targets[:, 1], mixed_s1.targets[:, 1]], dim=0)
 
-        x = torch.cat([x_s0, x_s1], dim=0)
-        y = torch.cat([y_s0, y_s1], dim=0)
+        # x = torch.cat([x_s0, x_s1], dim=0)
+        # y = torch.cat([y_s0, y_s1], dim=0)
 
         # mixed_out = self.forward(x=batch.x, s=batch.s)
         # mixed_pred_loss = torch.nn.functional.mse_loss(
@@ -286,34 +286,35 @@ class Clf(CommonModel):
         adv_loss = self.loss.adv_loss(clf_out, s=s)
         mmd_loss = self.loss.mmd_loss(clf_out, s=s)
 
-        x_s0y0 = batch.x[(batch.s == 0) & (batch.y == 0)]
-        x_s0y1 = batch.x[(batch.s == 0) & (batch.y == 1)]
-        x_s0 = torch.cat([x_s0y0, x_s0y1], dim=0)
-        x_s1y0 = batch.x[(batch.s == 1) & (batch.y == 0)]
-        x_s1y1 = batch.x[(batch.s == 1) & (batch.y == 1)]
-        x_s1 = torch.cat([x_s1y0, x_s1y1], dim=0)
-        s_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
-        s_s0y1 = batch.x.new_zeros((x_s0y1.shape[0]))
-        s_s0 = torch.cat([s_s0y0, s_s0y1], dim=0)
-        s_s1y0 = batch.x.new_ones((x_s1y0.shape[0]))
-        s_s1y1 = batch.x.new_ones((x_s1y1.shape[0]))
-        s_s1 = torch.cat([s_s1y0, s_s1y1], dim=0)
-        y_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
-        y_s0y1 = batch.x.new_ones((x_s0y1.shape[0]))
-        y_s0 = torch.cat([y_s0y0, y_s0y1], dim=0)
-        y_s1y0 = batch.x.new_zeros((x_s1y0.shape[0]))
-        y_s1y1 = batch.x.new_ones((x_s1y1.shape[0]))
-        y_s1 = torch.cat([y_s1y0, y_s1y1], dim=0)
+        # x_s0y0 = batch.x[(batch.s == 0) & (batch.y == 0)]
+        # x_s0y1 = batch.x[(batch.s == 0) & (batch.y == 1)]
+        # x_s0 = torch.cat([x_s0y0, x_s0y1], dim=0)
+        # x_s1y0 = batch.x[(batch.s == 1) & (batch.y == 0)]
+        # x_s1y1 = batch.x[(batch.s == 1) & (batch.y == 1)]
+        # x_s1 = torch.cat([x_s1y0, x_s1y1], dim=0)
+        # s_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
+        # s_s0y1 = batch.x.new_zeros((x_s0y1.shape[0]))
+        # s_s0 = torch.cat([s_s0y0, s_s0y1], dim=0)
+        # s_s1y0 = batch.x.new_ones((x_s1y0.shape[0]))
+        # s_s1y1 = batch.x.new_ones((x_s1y1.shape[0]))
+        # s_s1 = torch.cat([s_s1y0, s_s1y1], dim=0)
+        # y_s0y0 = batch.x.new_zeros((x_s0y0.shape[0]))
+        # y_s0y1 = batch.x.new_ones((x_s0y1.shape[0]))
+        # y_s0 = torch.cat([y_s0y0, y_s0y1], dim=0)
+        # y_s1y0 = batch.x.new_zeros((x_s1y0.shape[0]))
+        # y_s1y1 = batch.x.new_ones((x_s1y1.shape[0]))
+        # y_s1 = torch.cat([y_s1y0, y_s1y1], dim=0)
 
-        mixed_s0 = self.mixup_s0(x_s0, targets=y_s0.long())
-        mixed_s1 = self.mixup_s1(x_s1, targets=y_s1.long())
-        mixed_x = torch.cat([mixed_s0.inputs, mixed_s1.inputs], dim=0)
-        mixed_s = torch.cat([s_s0, s_s1], dim=0)
-        mixed_y = torch.cat([mixed_s0.targets[:, 1], mixed_s1.targets[:, 1]], dim=0)
+        # mixed_s0 = self.mixup_s0(x_s0, targets=y_s0.long())
+        # mixed_s1 = self.mixup_s1(x_s1, targets=y_s1.long())
+        # mixed_x = torch.cat([mixed_s0.inputs, mixed_s1.inputs], dim=0)
+        # mixed_s = torch.cat([s_s0, s_s1], dim=0)
+        # mixed_y = torch.cat([mixed_s0.targets[:, 1], mixed_s1.targets[:, 1]], dim=0)
 
-        mixed_out = self.forward(x=mixed_x, s=mixed_s)
+        mixed = self.mixup(x, targets=y, group_labels=s)
+        mixed_out = self.forward(x=mixed.inputs, s=s)
         mixed_pred_loss = torch.nn.functional.binary_cross_entropy_with_logits(
-            index_by_s(mixed_out.y, s).squeeze(), mixed_y
+            index_by_s(mixed_out.y, s).squeeze(), mixed.targets
         )
 
         loss = mixed_pred_loss + adv_loss + mmd_loss + pred_loss
